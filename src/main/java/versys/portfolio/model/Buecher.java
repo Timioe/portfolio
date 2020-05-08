678854fff8cf92ca966f7fceed0da7279f90137d
Timioe <55991668+Timioe@users.noreply.github.com>
2020-05-08 08:46:39 +0200

    Funktioniert NICHT
    SQL und ManyToMany verändert

--- src/main/java/versys/portfolio/model/Buecher.java
+++ src/main/java/versys/portfolio/model/Buecher.java
@@ -9,6 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.Min;
@@ -49,8 +50,9 @@ public class Buecher implements Serializable {
     @Lob
     private String description = "";
     
-    @ManyToOne
-    private Bibliothek bibliothek = null;
+    @ManyToMany
+    List<Bibliothek> bibliothek = new ArrayList<>();
+   
     
 
     
