c04598589fe2ce2b77d79d2fdd4f8128b4e21c9f
Timioe <55991668+Timioe@users.noreply.github.com>
2020-05-08 10:16:52 +0200

    Revert "Revert "Funktioniert""
    This reverts commit a854f062a13a58223c494bc50580373174f947c2.

--- src/main/resources/static/page.js
+++ src/main/resources/static/page.js
@@ -1,44 +1,53 @@
 const API_URL = "http://localhost:9090/api";
 
 window.addEventListener("load", async () => {
-	// Vorhandene Einträge abrufen und anzeigen
-	
-	//Bibliotheken auch dynamisch abrufen und anhand deren Namen dann getEntries aufrufen und evtl für jede Bib einen neuen div erzeugen, in den dann
-	//der Name der bib und die bücher in einer liste drunter gelegt werden können
-	getEntries('kit_ka');
-	getEntries('dhbw_ka')
+	// Vorhandene Bibliotheken abrufen und anzeigen
+	getBibs();
 
 	// Old-School Abschicken des Formulars unterdrücken
 	event.preventDefault();
 	return false;
 });
 
-async function getEntries(sName) {
-	if (sName.trim() === "") return;
-
+async function getBibs() {
 	// Backend-API aufrufen
-	let entry = await fetch(API_URL + "/bibliotheken/" + sName + "/buecher", {
+	let entry = await fetch(API_URL + "/bibliotheken/", {
 		method: "GET"
 	});
 
-		let oldName = "";
-	// Gespeicherten Eintrag anzeigen
+	// Gespeicherte BIbliotheken anzeigen
 	let entries = await entry.json();
-	entries._embedded.buecher.forEach(element => displayEntry(element, sName, oldName));
+	entries._embedded.bibliotheken.forEach(element => displayBibs(element));
 }
 
-function displayEntry(entry, sName, oldName) {
+async function displayBibs(entry) {
+	// Bibliothek anzeigen
 	let div = document.getElementById("data");
+	var h2 = document.createElement('h2');
+	h2.setAttribute("id", entry.name)
+	h2.appendChild(document.createTextNode(entry.name));
+	div.appendChild(h2);
 
-	if (sName != oldName) {
-		var h2 = document.createElement('h2');
-		h2.appendChild(document.createTextNode(sName));
-		div.appendChild(h2);
-		oldName = sName;
-	}
+	// Bücher der Bibliothek besorgen und anzeigen
+	// Backend-API aufrufen, URL durch href gesetzt
+	let entryBuch = await fetch(entry._links.self.href + "/buecher", {
+		method: "GET"
+	});
+
+	// Einträge in Konsole anzeigen
+	console.log(entryBuch);
+	
+	// Gespeicherten Eintrag anzeigen
+	let entries = await entryBuch.json();
+	entries._embedded.buecher.forEach(element => displayEntry(element, entry.name));
+
+}
+
+function displayEntry(entry, bibName) {
+	// Buch anzeigen
+	let h2 = document.getElementById(bibName);
 	var ul = document.createElement('ul');
-	div.appendChild(ul);
-
+	h2.appendChild(ul);
 	var item = document.createElement('li');
 	item.appendChild(document.createTextNode(entry.name));
 	ul.appendChild(item);
